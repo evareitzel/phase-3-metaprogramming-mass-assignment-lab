fd24def370a315b2f26264ecf2c756b441840972
Eva Reitzel <evareitzel@gmail.com>
2022-11-22 21:53:40 -0500

    commit tree 0aba4dfc451ae7eecfbf08e932c61ef0f990e9ff

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, val|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", val)
+    end
+  end
 end
+
+# initialize
+  # iteration
+  # send
